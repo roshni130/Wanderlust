378737fafe98005f89ba09c07ff5642ba98d8292
Roshni Kumari <kumariroshniraj840@gmail.com>
2024-09-24 22:26:52 +0530

    Add files

---
+++ models/listing.js
@@ -0,0 +1,56 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+const Review=require("./review.js");
+
+const listingSchema=new Schema({
+    title:{
+      type:String,
+      required:true
+    },
+    description:String,
+    // image:{
+    //   filename: String,
+    //   url:{
+    //     type:String,
+    //     default:"https://media.istockphoto.com/id/1171744277/photo/modern-house-with-pool-and-evening-lighting.jpg?s=2048x2048&w=is&k=20&c=vmnivJCs4UboHz-zHB9tuJiuB_CZBO6qrv1UvUNY3uM=",
+    //     set:(v) =>v=== "" ? "https://media.istockphoto.com/id/1171744277/photo/modern-house-with-pool-and-evening-lighting.jpg?s=2048x2048&w=is&k=20&c=vmnivJCs4UboHz-zHB9tuJiuB_CZBO6qrv1UvUNY3uM=" : v, 
+    //   }
+    // },
+    image:{
+      url:String,
+      filename:String,
+    },
+    price:Number,
+    location:String,
+    country:String,
+    reviews:[
+      {
+        type:Schema.Types.ObjectId,
+        ref:"Review"
+      },
+    ],
+    owner:{
+      type:Schema.Types.ObjectId,
+      ref:"User"
+    },
+    geometry:{
+      type:{
+        type:String,
+        enum:['Point'],
+        required:true
+      },
+      coordinates:{
+        type:[Number],
+        required:true
+      }
+    }
+});
+
+listingSchema.post("findOneAndDelete",async(listing)=>{
+  if(listing){
+    await Review.deleteMany({_id:{$in:listing.reviews}});
+  }
+});
+
+const Listing=mongoose.model("Listing",listingSchema);
+module.exports=Listing;
